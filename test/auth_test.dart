0c0c5ec66b26a20e2566093b2755d676bc658582
Kevin Xu <kevinxu9595.com>
2023-06-29 13:13:52 +0100

    step-19

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -102,6 +102,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'my_id',
       email: 'foo@bar.com',
       isEmailVerified: false,
     );
@@ -123,6 +124,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: 'my_id',
       email: 'foo@bar.com',
       isEmailVerified: true,
     );
